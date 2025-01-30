a216a6f350a316adaadb729b31ff477327afb7ec
Raedtselachtig <sraedts@gmail.com>
2025-01-30 16:51:40 +0100

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero'
+import Services from '../components/Services'
+import Features from '../components/Features'
+import Contact from '../components/Contact'
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Services />
+      <Features />
+      <Contact />
+    </main>
+  )
+}
